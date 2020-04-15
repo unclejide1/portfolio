2eb6d12853ecec747476f906994463c3fd5333c4
Jideofor <njusticej@gmail.com>
2020-04-15 12:13:24 +0100

    feat: add routing to pages

--- src/App.js
+++ src/App.js
@@ -1,59 +1,96 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import './App.css';
+import "./App.css";
 
-
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       title: "Jideofor Nwoke",
       headerLinks: [
-        {title: 'Home', path: "/"},
-        {title: 'About', path: "/about"},
-        {title: 'Contact', path: "/contact"}
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" },
       ],
       home: {
-        title: 'Never Stop Writing Awesome Code!',
-        subTitle: 'What have i Been working on?',
-        text: 'Checkout Below'
+        title: "Never Stop Writing Awesome Code!",
+        subTitle: "What have i Been working on?",
+        text: "Checkout Below",
       },
       about: {
-        title: "Who Am I?"
+        title: "Who Am I?",
       },
-      contact:{
-        title: 'Let\'s Talk More'
-      }
-    }
+      contact: {
+        title: "Let's Talk More",
+      },
+    };
   }
-  render(){
+  render() {
     return (
-      <Router >
+      <Router>
         <Container className="p-0" fluid={true}>
-          <Navbar className="border-bottom" bg ="transparent" expand ="lg">
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
             <Navbar.Brand>UncleJide</Navbar.Brand>
-          
-          <Navbar.Toggle className ="border-0 " aria-controls="navbar-toggle"/>
-          <Navbar.Collapse id ="navbar-toggle">
-            <Nav className="ml-auto">
-              <Link className="nav-link" to ="/">Home</Link>
-              <Link className="nav-link" to ="/about">About</Link>
-              <Link className="nav-link" to ="/contact">Contact</Link>
-            </Nav>
 
-          </Navbar.Collapse>
+            <Navbar.Toggle
+              className="border-0 "
+              aria-controls="navbar-toggle"
+            />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
-          <Footer/>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <HomePage
+                title={this.state.home.title}
+                subTitle={this.state.home.subTitle}
+                text={this.state.home.text}
+              />
+            )}
+          />
+          <Route
+            path="/about"
+            render={() => (
+              <AboutPage
+                title={this.state.about.title}
+                
+              />
+            )}
+          />
+          <Route
+            path="/contact"
+            render={() => (
+              <ContactPage
+                title={this.state.contact.title}
+              />
+            )}
+          />
+          <Footer />
         </Container>
       </Router>
     );
   }
-  
 }
 
 export default App;
