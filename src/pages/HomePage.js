210af9af14a9168e48e07a951ce24da0fc479590
Jideofor <njusticej@gmail.com>
2020-04-15 15:41:02 +0100

    feat: render hero component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 function HomePage(props){
 
     return(
-<p>Home page works</p>
+<Hero title= {props.title} subTitle = {props.subTitle} text = {props.text}/>
     );
 
 }
