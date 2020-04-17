3f5ddea851ceeb545f7213be9c963dd9d4e48548
Jideofor <njusticej@gmail.com>
2020-04-17 14:14:21 +0100

    feat: add card component

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+import CardInfo from "./CardInfo";
+
+function Card(props) {
+  return (
+    <div className = "d-inline-block j-card" onClick = {(e) => props.onClick(props.item)}>
+      <img className="j-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
+      {props.item.selected && (
+        <CardInfo
+          title={props.item.title}
+          subTitle={props.item.subTitle}
+          link={props.item.link}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Card;
