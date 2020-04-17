6cf2102adcaf9676adda41d28bcdbaca8d23a214
Jideofor <njusticej@gmail.com>
2020-04-17 14:14:55 +0100

    refactor: update card info component

---
+++ src/components/CardInfo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+
+function CardInfo(props) {
+  const style = useSpring({ opacity: 1, from: { opacity: 0 } });
+
+  return (
+    <animated.div className="j-card-info" style={style}>
+      <p className="j-card-title">{props.title}</p>
+      <p className = "j-card-sub-title">{props.subTitle}</p>
+      <a href={props.link} target="_blank" rel="noopener noreferrer">
+        View{" "}
+      </a>
+    </animated.div>
+  );
+}
+
+export default CardInfo;
